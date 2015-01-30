ef6c16398fb2b9b7c701c06eda524414ae45ef4c
Philipp Giese <giese.philipp+git@gmail.com>
2015-01-30 13:41:59 +0100

    Updates

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require.config({
   paths: {
     "react": "lib/vendor/react/build/react-with-addons",
     "react-mentions": "lib/react-mentions",
-    "react-router": "lib/react-router/dist/react-router",
+    "react-router": "lib/vendor/react-router/dist/react-router",
 
     "JSXTransformer": "lib/vendor/requirejs-jsx-plugin/js/JSXTransformer",
     "jsx": "lib/vendor/requirejs-jsx-plugin/js/jsx",

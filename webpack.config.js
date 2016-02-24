c7345d3264a9f1a57d63ff8495fe1c773888003b
Jan-Felix <schwarz@mpango.de>
2016-02-24 12:07:52 +0200

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel', exlude:/node_modules/ },
+      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       {
         test: /\.less$/,

958d87224fa93ac837fdf0a02680ce58f2f64a15
Jan-Felix <schwarz@mpango.de>
2016-02-24 12:36:32 +0200

    Updates

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -16,7 +16,25 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+      { 
+        test: /\.js$/, 
+        loader: 'babel', 
+        exclude: /node_modules/,
+        query: {
+          "plugins": [
+            ["react-transform", {
+              "transforms": [{
+                "transform": "react-transform-hmr",
+                "imports": ["react"],
+                "locals": ["module"]
+              }, {
+                "transform": "react-transform-catch-errors",
+                "imports": ["react", "redbox-react"]
+              }]
+            }]
+          ]
+        }
+      },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       {
         test: /\.less$/,

8883beed792499f7a6602970755d0be4691c8def
Philipp Giese <giese.philipp+git@gmail.com>
2016-03-24 11:49:13 +0100

    Updates

--- views/examples/defaultStyle.js
+++ views/examples/defaultStyle.js
@@ -4,43 +4,50 @@ export default (props={}) => ({
 
         fontSize: 12,
         fontWeight: "normal",
-
-        ...(props.singleLine
-            ? {
-                display: "inline-block",
-
-                width: 130,
-            }
-            : {
-                fontFamily: "monospace",
-
-                border: "1px solid silver",
-            }
-        )
     },
-    highlighter: {
-        ...(props.singleLine
-            ? {
-                padding: 1,
-                border: "2px inset transparent",
-            }
-            : {
-                padding: 9,
-            }
-        ),
-    },
-    textarea: {
-        padding: 9,
-        minHeight: 63,
-        outline: 0,
-        border: 0,
-        margin: 0,
-    },
+
     input: {
-        padding: 1,
         margin: 0,
-        border: "2px inset",
     },
+
+    "&singleLine": {
+        control: {
+            display: "inline-block",
+
+            width: 130,
+        },
+
+        highlighter: {
+            padding: 1,
+            border: "2px inset transparent",
+        },
+
+        input: {
+            padding: 1,
+
+            border: "2px inset",
+        },
+    },
+
+    "&multiLine": {
+        control: {
+            fontFamily: "monospace",
+
+            border: "1px solid silver",
+        },
+
+        highlighter: {
+            padding: 9,
+        },
+
+        input: {
+            padding: 9,
+            minHeight: 63,
+            outline: 0,
+            border: 0,
+        }
+    },
+
     suggestions: {
         border: "1px solid rgba(0,0,0,0.15)",
         fontSize: 10,

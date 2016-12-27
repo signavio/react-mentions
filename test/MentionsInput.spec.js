aaf64ae6815f3be38ebbce894064ca4e7ebca9da
Jan-Felix <schwarz@mpango.de>
2016-12-27 10:52:59 +0100

    fixed a bug when having multiple sync responses from different mentions data sources (state updates overwrite each other)

--- test/MentionsInput.spec.js
+++ test/MentionsInput.spec.js
@@ -6,7 +6,7 @@ import { MentionsInput, Mention } from "../src";
 
 const data = [
     { id: "first", value: "First entry" },
-    { id: "second", vallue: "Second entry" }
+    { id: "second", value: "Second entry" }
 ];
 
 describe("MentionsInput", () => {
@@ -41,5 +41,25 @@ describe("MentionsInput", () => {
     it("should be possible to navigate through the suggestions with the up and down arrows.");
     it("should be possible to select a suggestion with enter.");
     it("should be possible to close the suggestions with esc.");
+
+    it('should be able to handle sync responses from multiple mentions sources', () => {
+        const wrapper = mount(
+            <MentionsInput value="@">
+                <Mention trigger="@" type="testentries" data={data} />
+                <Mention
+                    trigger="@"
+                    type="testchars"
+                    data={[ { id: "a", value: "A" }, { id: "b", value: "B" } ]}
+                />
+            </MentionsInput>
+        )
+
+        wrapper.find("textarea").simulate('focus')
+        wrapper.find("textarea").simulate('select', {
+            target: { selectionStart: 1, selectionEnd: 1 }
+        })
+
+        expect(wrapper.find('SuggestionsOverlay').find('Suggestion')).to.have.length(4)
+    })
 
 });

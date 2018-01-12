bfb8a84e875b2de39a023ffef8d98b8d2898f051
Nikko Reyes <niknokseyer@gmail.com>
2018-01-12 14:49:02 -0800

    Added isRequired on oneOfType
    Change request completed: added isRequired on oneOfType (string and number)

--- src/Suggestion.js
+++ src/Suggestion.js
@@ -7,14 +7,14 @@ import keys from 'lodash/keys';
 class Suggestion extends Component {
 
   static propTypes = {
-    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     query: PropTypes.string.isRequired,
     index: PropTypes.number.isRequired,
 
     suggestion: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.shape({
-        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
         display: PropTypes.string
       }),
     ]).isRequired,

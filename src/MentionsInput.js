2389e2b317950cca807af81dfd391046e8aba7dc
Andre Sardo <a.fsardo@gmail.com>
2018-03-06 16:12:00 +0000

    feat: Sync scroll of highlighter (#227)
    * feat(Scrollable) : Add a scroll handler to support overflow: auto
    
    * chore(Scrollable) : Create scrollable example
    
    * refactor(Scrollable) : Less code to maintain
    
    * fix(Scrollable) : Fix issue with 2 visible overlapping scroll bars

--- src/MentionsInput.js
+++ src/MentionsInput.js
@@ -138,6 +138,7 @@ class MentionsInput extends React.Component {
           onBlur: this.handleBlur,
           onCompositionStart: this.handleCompositionStart,
           onCompositionEnd: this.handleCompositionEnd,
+          onScroll: this.updateHighlighterScroll,
         }),
     }
   }
@@ -474,6 +475,8 @@ class MentionsInput extends React.Component {
     const input = this.refs.input
     const highlighter = ReactDOM.findDOMNode(this.refs.highlighter)
     highlighter.scrollLeft = input.scrollLeft
+    highlighter.scrollTop = input.scrollTop
+    highlighter.height = input.height
   }
 
   handleCompositionStart = () => {

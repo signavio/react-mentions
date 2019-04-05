b2bb30f334d1da789120554ec758851e5f9edebb
Jan-Felix <janfelixschwarz@gmail.com>
2019-04-05 09:58:38 +0200

    added comment

--- src/utils/escapeRegex.js
+++ src/utils/escapeRegex.js
@@ -1,3 +1,4 @@
-const escapeRegex = str => str.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
+// escape RegExp special characters https://stackoverflow.com/a/9310752/5142490
+const escapeRegex = str => str.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
 
 export default escapeRegex

2c017bfc15d5989fba45d957680de700501187c6
Yannai Alter <YanayAlter@gmail.com>
2021-05-07 21:13:12 +0300

    Add an example

---
+++ demo/src/examples/CustomSuggestionsContainer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Mention, MentionsInput } from '../../../src'
+
+import { provideExampleValue } from './higher-order'
+import defaultStyle from './defaultStyle'
+import defaultMentionStyle from './defaultMentionStyle'
+
+function CustomSuggestionsContainer({ value, data, onChange, onAdd }) {
+  return (
+    <div className="single-line">
+      <h3>Custom Mentions Container</h3>
+
+      <MentionsInput
+        value={value}
+        onChange={onChange}
+        style={defaultStyle}
+        placeholder={"Mention people using '@'"}
+        a11ySuggestionsListLabel={"Suggested mentions"}
+        customSuggestionsContainer={(children)=><div>{children}<span>Customizing container</span></div>}
+      >
+        <Mention data={data} onAdd={onAdd} style={defaultMentionStyle} />
+      </MentionsInput>
+    </div>
+  )
+}
+
+const asExample = provideExampleValue('')
+
+export default asExample(CustomSuggestionsContainer)

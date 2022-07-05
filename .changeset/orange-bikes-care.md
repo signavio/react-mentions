6149b4474e49c137274747d42a9f31e4d38107ac
Sigrsig <sospsig@gmail.com>
2022-07-05 10:24:54 +0100

    Suggestion.js refactored to functional component

---
+++ .changeset/orange-bikes-care.md
@@ -0,0 +1,5 @@
+---
+"react-mentions": patch
+---
+
+Suggestions.js refactored to functional component
--- src/Suggestion.js
+++ src/Suggestion.js
@@ -1,54 +1,20 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import { defaultStyle } from './utils'
 
 import { getSubstringIndex, keys, omit } from './utils'
 
-class Suggestion extends Component {
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    query: PropTypes.string.isRequired,
-    index: PropTypes.number.isRequired,
-    ignoreAccents: PropTypes.bool,
+function Suggestion(props) {
+  const rest = omit(
+    props,
+    ['style', 'classNames', 'className'], // substyle props
+    keys(Suggestion.propTypes)
+  )
 
-    suggestion: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.shape({
-        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-          .isRequired,
-        display: PropTypes.string,
-      }),
-    ]).isRequired,
-    renderSuggestion: PropTypes.func,
+  const renderContent = () => {
+    let { query, renderSuggestion, suggestion, index, focused } = props
 
-    focused: PropTypes.bool,
-  }
-
-  render() {
-    const rest = omit(
-      this.props,
-      ['style', 'classNames', 'className'], // substyle props
-      keys(Suggestion.propTypes)
-    )
-
-    return (
-      <li
-        id={this.props.id}
-        role="option"
-        aria-selected={this.props.focused}
-        {...rest}
-        {...this.props.style}
-      >
-        {this.renderContent()}
-      </li>
-    )
-  }
-
-  renderContent() {
-    let { query, renderSuggestion, suggestion, index, focused } = this.props
-
-    let display = this.getDisplay()
-    let highlightedDisplay = this.renderHighlightedDisplay(display, query)
+    let display = getDisplay()
+    let highlightedDisplay = renderHighlightedDisplay(display, query)
 
     if (renderSuggestion) {
       return renderSuggestion(
@@ -63,8 +29,8 @@ class Suggestion extends Component {
     return highlightedDisplay
   }
 
-  getDisplay() {
-    let { suggestion } = this.props
+  const getDisplay = () => {
+    let { suggestion } = props
 
     if (typeof suggestion === 'string') {
       return suggestion
@@ -79,8 +45,8 @@ class Suggestion extends Component {
     return display
   }
 
-  renderHighlightedDisplay(display) {
-    const { ignoreAccents, query, style } = this.props
+  const renderHighlightedDisplay = (display) => {
+    const { ignoreAccents, query, style } = props
 
     let i = getSubstringIndex(display, query, ignoreAccents)
 
@@ -96,6 +62,18 @@ class Suggestion extends Component {
       </span>
     )
   }
+
+  return (
+    <li
+      id={props.id}
+      role="option"
+      aria-selected={props.focused}
+      {...rest}
+      {...props.style}
+    >
+      {renderContent()}
+    </li>
+  )
 }
 
 const styled = defaultStyle(

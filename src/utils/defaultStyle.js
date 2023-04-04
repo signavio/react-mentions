e174d9e3dc1f31f18ef841a2ffd4557122377f17
Remi Achji <achji.remi@live.com>
2023-04-04 11:40:42 +0400

    added ref

--- src/utils/defaultStyle.js
+++ src/utils/defaultStyle.js
@@ -2,30 +2,33 @@ import React from 'react'
 import useStyles from 'substyle'
 
 function createDefaultStyle(defaultStyle, getModifiers) {
-  const enhance = (ComponentToWrap) => {
-    const DefaultStyleEnhancer = ({
-      style,
-      className,
-      classNames,
-      ...rest
-    }) => {
-      const modifiers = getModifiers ? getModifiers(rest) : undefined
-      const styles = useStyles(
-        defaultStyle,
-        { style, className, classNames },
-        modifiers
-      )
+    const enhance = (ComponentToWrap) => {
+        const DefaultStyleEnhancer = ({
+            style,
+            className,
+            classNames,
+            ...rest
+        }) => {
+            const modifiers = getModifiers ? getModifiers(rest) : undefined
+            const styles = useStyles(
+                defaultStyle,
+                { style, className, classNames },
+                modifiers
+            )
 
-      return <ComponentToWrap {...rest} style={styles} />
+            return <ComponentToWrap {...rest} style={styles} />
+        }
+        const displayName =
+            ComponentToWrap.displayName || ComponentToWrap.name || 'Component'
+        DefaultStyleEnhancer.displayName = `defaultStyle(${displayName})`
+
+        // return DefaultStyleEnhancer
+        return React.forwardRef((props, ref) => {
+            return DefaultStyleEnhancer({ ...props, ref })
+        })
     }
-    const displayName =
-      ComponentToWrap.displayName || ComponentToWrap.name || 'Component'
-    DefaultStyleEnhancer.displayName = `defaultStyle(${displayName})`
 
-    return DefaultStyleEnhancer
-  }
-
-  return enhance
+    return enhance
 }
 
 export default createDefaultStyle
